5c91c2aca295cfeb118494114b72ffd358c25fa5
Morvarid <morvaridmahmoudi@gmail.com>
2022-07-18 15:40:56 +0200

    fetchinge before merging

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import MovieBox from "./components/MovieBox";
+import Movie from "./components/Movie";
 
 function App() {
-  const[movies,setMovies]=useState([])
-  // const  example = ["one", "two"]
 
 
-  useEffect (()=>{
-    fetch('http://localhost:3001/movie')
-    .then(res => res.json())
-    .then(data =>{ 
-        console.log(data);
-      setMovies(data.results);
-    })
-  
-  },[])
 
   
   
   return (
     <div className="App">
-      {movies.map(movie => 
-      <MovieBox key={movie.id} {...movie}/>)}
+      <Movie/>
+    
 
     </div>
   );
