d3c7e436ea3ddff2533cf765ab22b74da76ed0cf
Andrea Elvegård <monikavalkova10@gmail.com>
2022-07-19 11:20:17 +0200

    making update functionality

--- server/crud.js
+++ server/crud.js
@@ -2,10 +2,10 @@
 import { MongoClient } from 'mongodb';
 
 const PASSWORD = '1234SALT';
+const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
 
 async function main() {
 
-  const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
 
     const client = new MongoClient(uri);
 
@@ -16,7 +16,7 @@ async function main() {
         await createListing(client,
           {
             user: "user1@gmail.com",
-            liked_movie: [
+            liked_movies: [
               {
                 name: "The Karate Kid",
                 summary: "After some violent confrontations with his new classmates, Daniel LaRusso learns karate from Japanese handyman Mr. Miyagi, in order to defend himself.",
@@ -46,3 +46,26 @@ async function createListing(client, newListing){
   const result = await client.db("movies_db").collection("movie_collection").insertOne(newListing);
   console.log(`Added a new movie for user ${newListing.user} to the database!`);
 }
+
+const test = () => {
+  MongoClient.connect(uri, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("movies_db");
+  var myquery = { user: "user1@gmail.com" };
+  var newvalues = { $set: { liked_movies: {
+    name: "New Karate Kid",
+    summary: "After some violent confrontations with his new classmates, Daniel LaRusso learns karate from Japanese handyman Mr. Miyagi, in order to defend himself.",
+    img: 'someurlstringhere',
+    gene: 2,
+    userScore: 8.2,
+    id: 123129,
+  } } };
+  dbo.collection("movie_collection").updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+    db.close();
+  });
+  });
+}
+
+test();
