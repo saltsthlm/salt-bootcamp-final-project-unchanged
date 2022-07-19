5435fdb2df6a08e03c11f071806f3fefef67f802
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2022-07-19 15:52:01 +0200

    Did fetching genre

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -7,25 +7,19 @@ import Comedy from "./Pages/Comedy";
 import Drama from "./Pages/Drama";
 import Horror from "./Pages/Horror";
 import Romance from "./Pages/Romance";
-
-
-
-
 function Movies() {
-
   return (
     <div className="App">
     
       <Routes>
       <Route path="/" element={<Home />} />
-          <Route path="/action" element={<Action />} />
-          <Route path="/animate" element={<Animate />} />
-          <Route path="/comedy" element={<Comedy />} />
-          <Route path="/romance" element={<Romance />} />
-          <Route path="/horror" element={<Horror />} />
-          <Route path="/drama" element={<Drama />} />
+          <Route path="/28" element={<Action />} />
+          <Route path="/16" element={<Animate />} />
+          <Route path="/35" element={<Comedy />} />
+          <Route path="/10749" element={<Romance />} />
+          <Route path="/27" element={<Horror />} />
+          <Route path="/18" element={<Drama />} />
         </Routes>
-
     </div>
   );
 }
