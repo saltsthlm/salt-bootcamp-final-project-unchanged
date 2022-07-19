55b4c46d9656316f48bd10efe991f0b827b7d7a5
Andrea Elvegård <monikavalkova10@gmail.com>
2022-07-19 10:29:52 +0200

    Changing request

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LikeButton = ( movie ) => {
   console.log(movie);
   const [likedMovies, setLikedMovies ] = useState([]);
+  const { user } = useAuth0();
 
   useEffect(() => {
     // Send data to the backend via POST
@@ -10,9 +12,11 @@ const LikeButton = ( movie ) => {
     fetch('http://localhost:3001/likedMovieList', {  // Enter your IP address here
      method: 'POST', 
      mode: 'cors', 
-     body: JSON.stringify(likedMovies) // body data type must match "Content-Type" header
+     body: JSON.stringify({
+      user: user.email,
+      likedMovies: {...likedMovies}
+     }) // body data type must match "Content-Type" header
   })
-
   }, [likedMovies]);
 
   const handleLike = () => {
@@ -21,6 +25,7 @@ const LikeButton = ( movie ) => {
       image: movie.poster_path,
       category: [...movie.genre_ids],
       rating: movie.vote_average,
+      id: movie.id,
     }
     const newLikedMovies = [...likedMovies].push(newLikedMovie);
     setLikedMovies(newLikedMovies);
