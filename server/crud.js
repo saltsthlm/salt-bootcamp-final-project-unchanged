b66ae02dd848f04fb3ca2fc00d137a4b2fc73afb
nfluk <nickylukic@gmail.com>
2022-07-22 11:28:30 +0200

    added auth0 code in comments

--- server/crud.js
+++ server/crud.js
@@ -1,52 +1,17 @@
 import { MongoClient } from 'mongodb';
+import { useInRouterContext } from 'react-router-dom';
 
 
 const PASSWORD = '1234SALT';
 const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
 
-function create(user, callback) {
-  const bcrypt = require('bcrypt');
-  // const MongoClient = require('mongodb@3.1.4').MongoClient;
-  const client = uri;
-  client.connect(function (err) {
-    if (err) return callback(err);
-
-    const db = client.db('movies_db');
-    const users = db.collection('movie_collection');
-
-    users.findOne({ email: user.email }, function (err, withSameMail) {
-      if (err || withSameMail) {
-        client.close();
-        return callback(err || new Error('the user already exists'));
-      }
-
-      bcrypt.hash(user.password, 10, function (err, hash) {
-        if (err) {
-          client.close();
-          return callback(err);
-        }
-
-        user.password = hash;
-        users.insert(user, function (err, inserted) {
-          client.close();
-
-          if (err) return callback(err);
-          callback(null);
-        });
-      });
-    });
-  });
-}
-
-
 async function main() {
-    create(user);
     const client = new MongoClient(uri);
     try {
         await client.connect();
         await createListing(client,
           {
-            user: user.email,
+            user: "mory@new.address",
             liked_movies: [
             ],
             disliked_movies: [
@@ -88,3 +53,40 @@ const addMovie = () => {
 }
 
 // addMovie();
+
+
+// function create(user, callback) {
+//   const bcrypt = require('bcrypt');
+//   const MongoClient = require('mongodb@3.1.4').MongoClient;
+//   const client = new MongoClient('mongodb+srv://codeClub:1234SALT@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority');
+
+//   client.connect(function (err) {
+//     if (err) return callback(err);
+
+//     const db = client.db('movies_db');
+//     const users = db.collection('movie_collection');
+
+//     users.findOne({ email: user.email }, function (err, withSameMail) {
+//       if (err || withSameMail) {
+//         client.close();
+//         return callback(err || new Error('the user already exists'));
+//       }
+
+//       bcrypt.hash(user.password, 10, function (err, hash) {
+//         if (err) {
+//           client.close();
+//           return callback(err);
+//         }
+
+//         user.password = hash;
+//         users.insert(user, function (err, inserted) {
+//           client.close();
+
+//           if (err) return callback(err);
+//           callback(null);
+//         });
+//       });
+//     });
+//   });
+// }
+
