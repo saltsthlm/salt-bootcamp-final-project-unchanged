4ec8eb0e9bacfcb57f069443b893dd2960f46acd
nfluk <nickylukic@gmail.com>
2022-07-22 10:21:38 +0200

    test adding user

--- server/crud.js
+++ server/crud.js
@@ -4,13 +4,49 @@ import { MongoClient } from 'mongodb';
 const PASSWORD = '1234SALT';
 const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
 
+function create(user, callback) {
+  const bcrypt = require('bcrypt');
+  // const MongoClient = require('mongodb@3.1.4').MongoClient;
+  const client = uri;
+  client.connect(function (err) {
+    if (err) return callback(err);
+
+    const db = client.db('movies_db');
+    const users = db.collection('movie_collection');
+
+    users.findOne({ email: user.email }, function (err, withSameMail) {
+      if (err || withSameMail) {
+        client.close();
+        return callback(err || new Error('the user already exists'));
+      }
+
+      bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+          client.close();
+          return callback(err);
+        }
+
+        user.password = hash;
+        users.insert(user, function (err, inserted) {
+          client.close();
+
+          if (err) return callback(err);
+          callback(null);
+        });
+      });
+    });
+  });
+}
+
+
 async function main() {
+    create(user);
     const client = new MongoClient(uri);
     try {
         await client.connect();
         await createListing(client,
           {
-            user: "nikola.lukic@appliedtechnology.se",
+            user: user.email,
             liked_movies: [
             ],
             disliked_movies: [
