2ad9f944cd9feec19609e914de4933c242265a33
Andrea Elvegård <monikavalkova10@gmail.com>
2022-07-25 10:21:12 +0200

    Starting to come together

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,11 +7,11 @@ const Profile = () => {
 
   return (
     <div className="profile">
-      <h1>My Profile</h1>
-      <img src={user.picture} alt={user.name}/>
-      <h2>{user.name}</h2>
-      <p>Username: {user.email}</p>
-      <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
+      <h1 className="profile-heading">My Profile</h1>
+      <img className="profile-img" src={user.picture} alt={user.name}/>
+      <h2 className="profile-username" >{user.name}</h2>
+      <p>{user.email}</p>
+      <button className="logout-btn" onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
     </div>
   );
 };
